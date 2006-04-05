b97d18b92eab8e2114460e5970bfe64ed0563613
thomasilsche <thomasilsche>
2006-04-05 00:35:00 +0000

    fixed chdir

--- run_all_tests.php
+++ run_all_tests.php
@@ -1,17 +1,34 @@
 <?php
-// $Id: run_all_tests.php,v 1.1 2005/11/11 20:30:10 weitzman Exp $
+
+// $Id: run_all_tests.php,v 1.2 2006/04/05 00:35:00 thomasilsche Exp $
+
+
 
 /**
+
  * @file
+
  * Run all unit tests for all enabled modules.
+
  */
 
+chdir('../../');
+
 include_once './includes/bootstrap.inc';
+
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
+
+
 // If not in 'safe mode', increase the maximum execution time:
+
 if (!ini_get('safe_mode')) {
+
   set_time_limit(360);
+
 }
 
+
+
 simpletest_run_tests();
+
